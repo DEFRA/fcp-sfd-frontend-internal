f91b6ebbd6c5a86ca53d4feb93b00f4c1fb96147
John Watson <42574977+johnwatson484@users.noreply.github.com>
2026-02-05 14:21:40 +0000

    Switch signout to use logout hint param (#9)

--- test/unit/auth/get-sign-out-url.test.js
+++ test/unit/auth/get-sign-out-url.test.js
@@ -22,7 +22,7 @@ vi.mock('../../../src/config/index.js', () => ({
 const { getSignOutUrl } = await import('../../../src/auth/get-sign-out-url.js')
 
 const request = { mock: 'request' }
-const token = 'ENTRA-ID-JWT'
+const loginHint = 'Entra-Login-Hint'
 
 describe('getSignOutUrl', () => {
   beforeEach(() => {
@@ -33,60 +33,60 @@ describe('getSignOutUrl', () => {
   })
 
   test('should get oidc config', async () => {
-    await getSignOutUrl(request, token)
+    await getSignOutUrl(request, loginHint)
     expect(mockGetOidcConfig).toHaveBeenCalled()
   })
 
   test('should create state from request', async () => {
-    await getSignOutUrl(request, token)
+    await getSignOutUrl(request, loginHint)
     expect(mockCreateState).toHaveBeenCalledWith(request)
   })
 
   test('should get redirect url from config', async () => {
-    await getSignOutUrl(request, token)
+    await getSignOutUrl(request, loginHint)
     expect(mockConfigGet).toHaveBeenCalledWith('entra.signOutRedirectUrl')
   })
 
   test('should return url with post_logout_redirect_uri as sign out url from config', async () => {
-    const result = await getSignOutUrl(request, token)
+    const result = await getSignOutUrl(request, loginHint)
     expect(result).toContain(`post_logout_redirect_uri=${mockSignOutRedirectUrl}`)
   })
 
-  test('should return url with id_token_hint token', async () => {
-    const result = await getSignOutUrl(request, token)
-    expect(result).toContain(`id_token_hint=${token}`)
+  test('should return url with logout_hint as login hint', async () => {
+    const result = await getSignOutUrl(request, loginHint)
+    expect(result).toContain(`logout_hint=${loginHint}`)
   })
 
   test('should return url with created state', async () => {
-    const result = await getSignOutUrl(request, token)
+    const result = await getSignOutUrl(request, loginHint)
     expect(result).toContain('state=mock-state')
   })
 
   test('should return url with all parameters encoded', async () => {
     mockCreateState.mockReturnValue('mock state')
-    const result = await getSignOutUrl(request, token)
+    const result = await getSignOutUrl(request, loginHint)
     expect(result).toContain('state=mock%20state')
   })
 
   test('should return all properties in correctly formatted url', async () => {
-    const result = await getSignOutUrl(request, token)
+    const result = await getSignOutUrl(request, loginHint)
     const url = new URL(result)
     const params = new URLSearchParams(url.searchParams)
     expect(url.protocol).toBe('https:')
     expect(url.host).toBe('example.com')
     expect(url.pathname).toBe('/sign-out')
     expect(params.get('post_logout_redirect_uri')).toBe(mockSignOutRedirectUrl)
-    expect(params.get('id_token_hint')).toBe(token)
+    expect(params.get('logout_hint')).toBe(loginHint)
     expect(params.get('state')).toBe('mock-state')
   })
 
   test('should throw an error if oidc config request fails', async () => {
     mockGetOidcConfig.mockRejectedValue(new Error('Test error'))
-    await expect(getSignOutUrl(request, token)).rejects.toThrow('Test error')
+    await expect(getSignOutUrl(request, loginHint)).rejects.toThrow('Test error')
   })
 
   test('should throw an error if state creation fails', async () => {
     mockCreateState.mockImplementation(() => { throw new Error('Test error') })
-    await expect(getSignOutUrl(request, token)).rejects.toThrow('Test error')
+    await expect(getSignOutUrl(request, loginHint)).rejects.toThrow('Test error')
   })
 })

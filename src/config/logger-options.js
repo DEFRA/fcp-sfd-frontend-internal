19dddd4b9cc21f454069ce24459a39ccea6b589f
Rebecca Ransome <beckyrose200@aol.com>
2026-06-02 11:47:46 +0100

    Add: Reduced logging noise

--- src/config/logger-options.js
+++ src/config/logger-options.js
@@ -1,6 +1,7 @@
 import { ecsFormat } from '@elastic/ecs-pino-format'
 import { getTraceId } from '@defra/hapi-tracing'
 import { config } from './index.js'
+const isLocal = config.get('server.isDevelopment')
 
 const logConfig = config.get('server.log')
 const serviceName = config.get('server.serviceName')
@@ -18,12 +19,24 @@ const formatters = {
 
 export const loggerOptions = {
   enabled: logConfig.enabled,
-  ignorePaths: ['/health'],
+  ignorePaths: isLocal ? ['/health', '/public', '/favicon.ico'] : ['/health'],
   redact: {
     paths: logConfig.redact,
     remove: true
   },
   level: logConfig.level,
+  // Local development logger settings
+  ...(isLocal && {
+    serializers: {
+      req: req => ({
+        method: req.method,
+        url: req.url
+      }),
+      res: res => ({
+        statusCode: res.statusCode
+      })
+    }
+  }),
   ...formatters[logConfig.format],
   nesting: true,
   mixin: () => {

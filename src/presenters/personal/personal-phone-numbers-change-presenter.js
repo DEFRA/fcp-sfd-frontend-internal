3bb1d99311d0625cffe74f7e4e7caeca178b8b79
Adam Kay <thousand_leaves@hotmail.com>
2026-07-27 13:38:44 +0100

    Add personal phone numbers change journey

---
+++ src/presenters/personal/personal-phone-numbers-change-presenter.js
@@ -0,0 +1,29 @@
+import { presenters } from '@defra/fcp-sfd-frontend-engine'
+
+/*
+ * Formats data ready for presenting in the `/customer/{CRN}/account-phone-numbers-change` page
+ * @module personalPhoneNumbersChangePresenter
+ */
+
+const personalPhoneNumbersChangePresenter = (personalDetails, payload, crn) => {
+  return {
+    backLink: { href: crn ? `/customer/${crn}/details` : '/search-crn' },
+    pageTitle: 'What are your personal phone numbers?',
+    metaDescription: 'Update the phone numbers for your personal account.',
+    userName: personalDetails.info.userName ?? null,
+    personalTelephone: presenters.formatNumber(
+      payload?.personalTelephone,
+      personalDetails.changePersonalPhoneNumbers?.personalTelephone,
+      personalDetails.contact.telephone
+    ),
+    personalMobile: presenters.formatNumber(
+      payload?.personalMobile,
+      personalDetails.changePersonalPhoneNumbers?.personalMobile,
+      personalDetails.contact.mobile
+    )
+  }
+}
+
+export {
+  personalPhoneNumbersChangePresenter
+}
---
+++ test/unit/presenters/personal/personal-phone-numbers-check-presenter.test.js
@@ -0,0 +1,100 @@
+// Test framework dependencies
+import { describe, test, expect, beforeEach } from 'vitest'
+
+// Thing under test
+import { personalPhoneNumbersCheckPresenter } from '../../../../src/presenters/personal/personal-phone-numbers-check-presenter.js'
+
+describe('personalPhoneNumbersCheckPresenter', () => {
+  let data
+  const crn = '1234567890'
+
+  beforeEach(() => {
+    data = {
+      info: {
+        userName: 'John Doe',
+        fullName: {
+          first: 'John',
+          last: 'Doe'
+        }
+      },
+      contact: {
+        telephone: '01111111111',
+        mobile: '02222222222'
+      },
+      changePersonalPhoneNumbers: {
+        personalTelephone: '01234567890',
+        personalMobile: '07123456789'
+      }
+    }
+  })
+
+  describe('when provided with personal phone numbers change data', () => {
+    test('it correctly presents the data', () => {
+      const result = personalPhoneNumbersCheckPresenter(data, crn)
+
+      expect(result).toEqual({
+        backLink: { href: '/customer/1234567890/account-phone-numbers-change' },
+        changeLink: '/customer/1234567890/account-phone-numbers-change',
+        pageTitle: 'Check your personal phone numbers are correct before submitting',
+        metaDescription: 'Check the phone numbers for your personal account are correct.',
+        userName: 'John Doe',
+        personalTelephone: {
+          telephone: '01234567890',
+          mobile: '07123456789'
+        }
+      })
+    })
+  })
+
+  describe('the "userName" property', () => {
+    describe('when the userName property is missing', () => {
+      beforeEach(() => {
+        delete data.info.userName
+      })
+
+      test('it should return userName as null', () => {
+        const result = personalPhoneNumbersCheckPresenter(data, crn)
+
+        expect(result.userName).toEqual(null)
+      })
+    })
+  })
+
+  describe('the "personalTelephone" property', () => {
+    describe('when a phone number is not provided', () => {
+      beforeEach(() => {
+        data.changePersonalPhoneNumbers = {
+          personalTelephone: null,
+          personalMobile: '07123456789'
+        }
+      })
+
+      test('it should return the missing number as null', () => {
+        const result = personalPhoneNumbersCheckPresenter(data, crn)
+
+        expect(result.personalTelephone).toEqual({
+          telephone: null,
+          mobile: '07123456789'
+        })
+      })
+    })
+
+    describe('when the mobile number is not provided', () => {
+      beforeEach(() => {
+        data.changePersonalPhoneNumbers = {
+          personalTelephone: '01234567890',
+          personalMobile: null
+        }
+      })
+
+      test('it should return the missing number as null', () => {
+        const result = personalPhoneNumbersCheckPresenter(data, crn)
+
+        expect(result.personalTelephone).toEqual({
+          telephone: '01234567890',
+          mobile: null
+        })
+      })
+    })
+  })
+})

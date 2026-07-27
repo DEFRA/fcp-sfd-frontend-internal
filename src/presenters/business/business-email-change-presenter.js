83443966f39727801ad2f7aa1022678a1f362605
Rana Salem <ranatasalem@gmail.com>
2026-07-27 13:23:51 +0100

    restore business email change/check presenters

--- src/presenters/business/business-email-change-presenter.js
+++ src/presenters/business/business-email-change-presenter.js
@@ -3,18 +3,23 @@
  * @module businessEmailChangePresenter
  */
 
-import { presenters } from '@defra/fcp-sfd-frontend-engine'
 import { resolveBackLink } from '../base-presenter.js'
 
 const businessEmailChangePresenter = (data, payload, referrer) => {
   const fallbackHref = data.info?.sbi ? `/business/${data.info.sbi}/details` : '/search-sbi'
 
-  const backLink = {
-    backLink: true,
-    href: resolveBackLink(referrer, fallbackHref)
+  return {
+    backLink: {
+      backLink: true,
+      href: resolveBackLink(referrer, fallbackHref)
+    },
+    pageTitle: 'What is your business email address?',
+    metaDescription: 'Update the email address for your business.',
+    userName: data.customer?.userName ?? null,
+    businessEmail: payload ?? data.changeBusinessEmail ?? data.contact.email,
+    businessName: data.info.businessName ?? null,
+    sbi: data.info.sbi ?? null
   }
-
-  return presenters.businessEmailChange(data, payload, backLink)
 }
 
 export { businessEmailChangePresenter }
--- src/presenters/business/business-email-check-presenter.js
+++ src/presenters/business/business-email-check-presenter.js
@@ -3,21 +3,25 @@
  * @module businessEmailCheckPresenter
  */
 
-import { presenters } from '@defra/fcp-sfd-frontend-engine'
 import { resolveBackLink } from '../base-presenter.js'
 import { BUSINESS_CHANGE_LINKS } from '../../constants/change-links.js'
 
 const businessEmailCheckPresenter = (data, referrer) => {
   const fallbackHref = data.info?.sbi ? BUSINESS_CHANGE_LINKS.businessEmail(data.info.sbi) : '/search-sbi'
 
-  const backLink = {
-    backLink: true,
-    href: resolveBackLink(referrer, fallbackHref)
+  return {
+    backLink: {
+      backLink: true,
+      href: resolveBackLink(referrer, fallbackHref)
+    },
+    changeLink: BUSINESS_CHANGE_LINKS.businessEmail(data.info.sbi),
+    pageTitle: 'Check your business email address is correct before submitting',
+    metaDescription: 'Check the email address for your business is correct.',
+    userName: data.customer?.userName ?? null,
+    businessEmail: data.changeBusinessEmail ?? data.contact.email,
+    businessName: data.info.businessName ?? null,
+    sbi: data.info.sbi ?? null
   }
-
-  const changeLink = BUSINESS_CHANGE_LINKS.businessEmail(data.info.sbi)
-
-  return presenters.businessEmailCheck(data, backLink, changeLink)
 }
 
 export { businessEmailCheckPresenter }

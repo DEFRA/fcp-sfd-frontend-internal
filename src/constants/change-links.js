fe78e54a6e9bb40357125bc4dd1e72dd7def6b5e
Rebecca Ransome <beckyrose200@aol.com>
2026-07-31 10:37:52 +0100

    Add: Config files for OS Places

--- src/constants/change-links.js
+++ src/constants/change-links.js
@@ -1,4 +1,11 @@
 export const BUSINESS_CHANGE_LINKS = {
   businessEmail: (sbi) => `/business/${sbi}/business-email-change`,
-  businessName: (sbi) => `/business/${sbi}/business-name-change`
+  businessName: (sbi) => `/business/${sbi}/business-name-change`,
+  businessAddress: (sbi) => `/business/${sbi}/business-address-change`
+}
+
+export const PERSONAL_CHANGE_LINKS = {
+  personalEmail: (crn) => `/customer/${crn}/account-email-change`,
+  personalName: (crn) => `/customer/${crn}/account-name-change`,
+  personalAddress: (crn) => `/customer/${crn}/account-address-change`
 }

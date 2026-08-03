3cd1e7b8eaf386e1d1079aac4b84a6f5a2ae25e5
Rana Salem <ranatasalem@gmail.com>
2026-08-03 12:13:45 +0100

    add missing comma to routes registration file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -57,7 +57,7 @@ export const routes = [
   ...businessNameChangeRoutes,
   ...businessNameCheckRoutes,
   ...businessPhoneNumbersChangeRoutes,
-  ...businessPhoneNumbersCheckRoutes
+  ...businessPhoneNumbersCheckRoutes,
   ...personalAddressEnterRoutes,
   ...personalAddressSelectRoutes,
   ...personalAddressCheckRoutes,

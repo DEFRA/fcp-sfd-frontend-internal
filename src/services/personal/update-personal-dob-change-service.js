517d379355b66697ff06792ca5a886520e3e3912
Adam Kay <thousand_leaves@hotmail.com>
2026-08-05 14:28:07 +0100

    Remove unused import of utils from updatePersonalDobChangeService

--- src/services/personal/update-personal-dob-change-service.js
+++ src/services/personal/update-personal-dob-change-service.js
@@ -9,7 +9,7 @@
  * @module updatePersonalDobChangeService
  */
 
-import { constants, mutations, utils } from '@defra/fcp-sfd-frontend-engine'
+import { constants, mutations } from '@defra/fcp-sfd-frontend-engine'
 
 import { fetchPersonalChangeService } from './fetch-personal-change-service.js'
 import { flashNotification } from '../../utils/notifications/flash-notification.js'

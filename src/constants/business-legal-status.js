459cc82ca05bd074a0ab4509659214adee905aae
Rebecca Ransome <beckyrose200@aol.com>
2026-08-20 11:17:24 +0100

    Add: Legal status constants

---
+++ src/constants/business-legal-status.js
@@ -0,0 +1,58 @@
+export const BUSINESS_LEGAL_STATUS = {
+  charitableIncorporatedOrganisation: {
+    text: 'Charitable incorporated organisation (CIO)',
+    code: '102101'
+  },
+  charitableTrust: {
+    text: 'Charitable trust',
+    code: '102113'
+  },
+  communityInterestCompany: {
+    text: 'Community interest company (CIC)',
+    code: '102102'
+  },
+  governmentCentral: {
+    text: 'Government (central)',
+    code: '102103'
+  },
+  governmentLocal: {
+    text: 'Government (local)',
+    code: '102104'
+  },
+  limitedLiabilityPartnership: {
+    text: 'Limited liability partnership (LLP)',
+    code: '102105'
+  },
+  limitedPartnership: {
+    text: 'Limited partnership',
+    code: '102106'
+  },
+  nonUkCompany: {
+    text: 'Non-UK company',
+    code: '102107'
+  },
+  partnership: {
+    text: 'Partnership',
+    code: '102108'
+  },
+  privateLimitedCompany: {
+    text: 'Private limited company (Ltd)',
+    code: '102109'
+  },
+  publicLimitedCompany: {
+    text: 'Public limited company (PLC)',
+    code: '102110'
+  },
+  soleProprietorship: {
+    text: 'Sole proprietorship',
+    code: '102111'
+  },
+  crown: {
+    text: 'The Crown',
+    code: '102112'
+  },
+  unlimitedCompany: {
+    text: 'Unlimited company (Ultd)',
+    code: '102114'
+  },
+}
